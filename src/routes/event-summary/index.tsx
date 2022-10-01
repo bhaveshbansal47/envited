20112b951d3ec741e2d7f97bf341ab18c36cb869
bhavesh-appbrew <103409900+bhavesh-appbrew@users.noreply.github.com>
2022-10-01 14:20:48 +0530

    fix: last style fix for bigger screens

--- src/routes/event-summary/index.tsx
+++ src/routes/event-summary/index.tsx
@@ -6,7 +6,7 @@ import { useEvent } from "../../App";
 export function EventSummary() {
   const [event] = useEvent();
   return (
-    <Flex style={{ flexDirection: "column" }}>
+    <Flex style={{ flexDirection: "column", alignItems: 'center' }}>
       <Image
         src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/17d6299f-f287-469c-a403-b8ab9d75aa62/Birthday_cake.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221001%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221001T075341Z&X-Amz-Expires=86400&X-Amz-Signature=61bf2112ffc9931d8ca424020d6b5d2049d94e7d422b306bfd4cee16780d2d7d&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Birthday%2520cake.png%22&x-id=GetObject"
         maxH={593}
@@ -14,7 +14,7 @@ export function EventSummary() {
         width="100%"
         height={"100%"}
       />
-      <Flex style={{ flexDirection: "column", padding: 20 }}>
+      <Flex style={{ flexDirection: "column", padding: 20, maxWidth: 593, width: '100%' }}>
         <Flex style={{ flexDirection: "column", marginBottom: 32 }}>
           <Text
             color={"primary.purpleDark"}
